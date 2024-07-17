50e4597dfa6f71034eaf58fe9982ee92143212d7
Lasha Kilasonia <lkilasonia@hotmail.com>
2024-07-18 00:34:39 +0400

    bersion 3.0

--- FunPlugin.php
+++ FunPlugin.php
@@ -3,7 +3,7 @@
 * Plugin Name: Updater
 * Plugin URI: https://github.com/Lkilasonia/updater
 * Description: This is a Fun Plugin.
-* Version: 2.0
+* Version: 3.0
 * Author: Lasha Kilasonia
 * Author URI: https://elementar.ge
 **/
@@ -15,13 +15,8 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	'FunPlugin'
 );
 
-// Load environment variables
-require_once __DIR__ . '/vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
 //Set the branch that contains the stable release.
 $myUpdateChecker->setBranch('main');
 
-// Optional: If you're using a private repository, specify the access token
-$myUpdateChecker->setAuthentication(getenv('GITHUB_ACCESS_TOKEN'));
+//Optional: If you're using a private repository, specify the access token like this:
+$myUpdateChecker->setAuthentication('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');
