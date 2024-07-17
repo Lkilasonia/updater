60b18bd1ba99c95922de168a17d3ed5d75a70693
Lasha Kilasonia <lkilasonia@hotmail.com>
2024-07-18 00:16:31 +0400

    bersion 2.0

--- FunPlugin.php
+++ FunPlugin.php
@@ -3,7 +3,7 @@
 * Plugin Name: Updater
 * Plugin URI: https://github.com/Lkilasonia/updater
 * Description: This is a Fun Plugin.
-* Version: 1.0
+* Version: 2.0
 * Author: Lasha Kilasonia
 * Author URI: https://elementar.ge
 **/
